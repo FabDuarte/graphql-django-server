e552625ebd768cff2c62b5f701252c2c71b12e6a
Fabricio Duarte <fabricio@asap.uy>
2018-01-21 23:30:06 -0300

    Initial commit

---
+++ starwars/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Planet, Film, Hero, Starship, Vehicle, People, Species, Transport
+
+
+admin.site.register(Planet)
+admin.site.register(Film)
+admin.site.register(Hero)
+admin.site.register(Starship)
+admin.site.register(Vehicle)
+admin.site.register(People)
+admin.site.register(Species)
+admin.site.register(Transport)
